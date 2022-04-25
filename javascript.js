ff580c63ca67228d98a6abe3cd1d54a49ed2420b
Filip50n <nic990000@gmail.com>
2022-04-25 17:26:05 +0200

    inicjalizacja repozytorium

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function test(){
+    console.log("zaczynamy")
+}
+
+// ----------------------------------------  == vs ===  ------------------------------
+
+//Operatory porównania
+
+function operatoryPorownania(){
+    console.log("operatory porównania == vs ===")
+}
